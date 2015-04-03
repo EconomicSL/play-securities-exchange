a7a2a38e032b3818ef7b9a3e9a5d9d0ed5d60e9e
David R. Pugh <drobert.pugh@gmail.com>
2015-04-03 15:28:42 +0100

    Fixed typo in docs.

--- app/models/LimitPriceLike.scala
+++ app/models/LimitPriceLike.scala
@@ -1,8 +1,8 @@
 package models
 
-/** Trait representing a limit order for a particular security.
+/** Trait representing a limit price for a particular security.
   *
-  * The LimitOrderLike trait should be mixed-in with both AskOrderLike and BidOrderLike
+  * The LimitPriceLike trait should be mixed-in with both AskOrderLike and BidOrderLike
   * traits to create instance of LimitAskOrder and LimitBidOrder classes.
   */
 trait LimitPriceLike {
