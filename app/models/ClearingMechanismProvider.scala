bffd9b36728137a262ad1e5b2d0754cfc2256563
David R. Pugh <drobert.pugh@gmail.com>
2015-04-29 22:12:27 +0100

    Added classes that provide auction and clearing mechanisms to markets depending on information in a configuration file.

---
+++ app/models/ClearingMechanismProvider.scala
@@ -0,0 +1,31 @@
+/*
+Copyright 2015 David R. Pugh
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package models
+
+import akka.actor.{Props, ActorRef, Actor, ActorLogging}
+import com.typesafe.config.ConfigFactory
+
+
+trait ClearingMechanismProvider {
+  this: Actor with ActorLogging =>
+
+  private val conf = ConfigFactory.load("markets.conf")
+  private val clearingMechanismClass = Class.forName(conf.getString("clearing-mechanism"))
+
+  val clearingMechanism: ActorRef = context.actorOf(Props(clearingMechanismClass), "clearing-mechanism")
+
+}
